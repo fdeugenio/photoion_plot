e960e5c08b98e9a4c3a75f4aaed70a76a8149db5
Francesco D'Eugenio <fdeugenio@gmail.com>
2023-06-29 14:32:42 +0100

    Added agreement at the start

--- harkatz_models/__init__.py
+++ harkatz_models/__init__.py
@@ -1,2 +1,11 @@
+import sys
 from .harkatz_models import *
 from .example_plot import *
+
+agree = input(
+    'These are models provided by A. Cameron on behalf of H. Katz as courtesy '
+    'to R. Tripodi and F. D\'Eugenio. Please ensure you have permission from '
+    'at least one of these four people before proceeding. Thank you.\n'
+    'Enter y to continue, any other key to stop\n')
+if not agree=='y':
+    sys.exit()
